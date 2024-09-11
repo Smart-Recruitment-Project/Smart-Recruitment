d7dce210c748da16e5755c455f0ea225fbf79ea5
RitikaGupta2235 <ritikagupta2235@gmail.com>
2024-09-11 15:05:21 +0530

    college components

--- client/src/College/Components/Navbar.js
+++ client/src/College/Components/Navbar.js
@@ -4,9 +4,9 @@ import avatar from '../../images/avatar.png';
 
 export default function Navbar({ title, onLogout }) {
   return (
-    <nav className="bg-customBlue min-w-full flex flex-row flex-wrap justify-between items-center border border-gray-600 shadow-lg rounded-lg z-50 p-3">
+    <nav className="bg-[#2f344a] min-w-full flex flex-row flex-wrap justify-between items-center border border-gray-600 shadow-lg rounded-lg z-50 p-3">
       <img className="h-16 ml-10" src={logo} alt="logo" />
-      <div className="title text-yellow-400 text-2xl font-bold">{title}</div>
+      <div className="title text-white text-2xl font-bold">{title}</div>
       <img className="h-16 mr-10 cursor-pointer" src={avatar} alt="avatar" onClick={onLogout} />
     </nav>
   );
--- client/src/College/Components/SideMenu.js
+++ client/src/College/Components/SideMenu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 export default function SideMenu({ onSelect }) {
   return (
-    <div class=" bg-customBlue text-white w-72 border-r border-gray-600 rounded-lg ml-10 p-20 justify-self-center " style={{ height: 'calc(100vh - 10rem)' }}>
+    <div class=" bg-[#2f344a] text-white w-72 border-r border-gray-600 rounded-lg ml-10 p-20 justify-self-center " style={{ height: 'calc(100vh - 10rem)' }}>
       <div class="flex justify-center  flex-col  text-xl font-bold">
         <button class="m-5 text-lg hover:text-yellow-400 hover:scale-150 transition duration-300 hover:ease-in-out" onClick={() => onSelect('Feeds')}>Feeds</button>
         <button class="m-5 text-lg hover:text-yellow-400 hover:scale-150 transition duration-300 hover:ease-in-out" onClick={() => onSelect('View Companies')}>View Companies</button>
